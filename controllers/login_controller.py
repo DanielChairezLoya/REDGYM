6f8a226d038a31b862a9e2ac621b98f7f50585ba
DanielChairezLoya <danielex7788@gmail.com>
2026-03-11 22:07:45 -0600

    Avance

---
+++ controllers/login_controller.py
@@ -0,0 +1,20 @@
+from PyQt6 import QtWidgets, uic
+
+class LoginController:
+    def __init__(self, window, model):
+        self.window = window
+        self.model = model
+        self.window.btn_login.clicked.connect(self.handle_login)
+        
+    def handle_login(self):
+        username = self.window.txt_username.text()
+        password = self.window.txt_password.text()
+        if username== "admin" and password=="1234":
+            self.window.login_succesfull.emit()
+            print("Login button clicked ")
+        else:
+            QtWidgets.QMessageBox.warning(
+                self.window,
+                "Abarrotes TEC -ERROR",
+                "Login incorrecto 😡"
+            )
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from PyQt6 import QtWidgets, uic
 import sys
-#from controllers.login_controller import LoginController
+from controllers.login_controller import LoginController
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtGui import QPalette
 
 class Login(QtWidgets.QMainWindow):
-    #login_succesfull=pyqtSignal()
+    login_succesfull=pyqtSignal()
     
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/login.ui",self)
+        self.controller = LoginController(self,self)
         
-        
+class Menu(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./views/menu.ui",self)        
         
 class AppManager:
     def __init__(self):
         self.login_window=Login()
+        self.menu_window=Menu()
+        self.login_window.login_succesfull.connect(self.show_main_window)
         
         self.login_window.show()
+        
+    def show_main_window(self):
+        self.menu_window.show()
+        self.login_window.close()    
         
 app = QtWidgets.QApplication(sys.argv)
 manager = AppManager()
