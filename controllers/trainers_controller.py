ce2836440f5b079325c7224a7acdf183a79dd1f1
DanielChairezLoya <danielex7788@gmail.com>
2026-03-13 17:47:32 -0600

    EvaluaciónFinal

---
+++ controllers/trainers_controller.py
@@ -0,0 +1,10 @@
+from PyQt6 import QtWidgets, uic
+
+class TrainerController:
+    def __init__(self, window, model):
+        self.window = window
+        self.model = model
+        self.window.btn_back.clicked.connect(self.menu_back)
+        
+    def menu_back(self):
+        self.window.back_menu.emit()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PyQt6 import QtWidgets, uic
 import sys
 from controllers.login_controller import LoginController
 from controllers.menu_controller import MenuController
+from controllers.trainers_controller import TrainerController
+from controllers.report_controller import ReportController
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtGui import QPalette
 
@@ -23,23 +25,51 @@ class Menu(QtWidgets.QMainWindow):
         self.controller = MenuController(self, self) 
         
 class Trainer(QtWidgets.QMainWindow):
+    back_menu=pyqtSignal()
+    
     def __init__(self):
         super().__init__()
-        uic.loadUi("./views/trainers.ui",self)       
+        uic.loadUi("./views/trainers.ui",self)
+        self.controller = TrainerController(self, self) 
+ 
+class Reports(QtWidgets.QMainWindow):
+    back_menu=pyqtSignal()
+    
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./views/report.ui",self)
+        self.controller = ReportController(self, self)              
         
 class AppManager:
     def __init__(self):
         self.login_window=Login()
         self.menu_window=Menu()
         self.trainer_window=Trainer()
+        self.report_window=Reports()
         self.login_window.login_succesfull.connect(self.show_main_window)
-        
+
         self.menu_window.trainers.connect(self.show_trainer_window)
+        self.menu_window.reportar.connect(self.show_report_window)
+        
+        self.trainer_window.back_menu.connect(self.back_trainer_window)
+        self.report_window.back_menu.connect(self.back_report_window)
         
         self.login_window.show()
     
+    def back_report_window(self):
+        self.menu_window.show()
+        self.report_window.close()
+        
+    def back_trainer_window(self):
+        self.menu_window.show()
+        self.trainer_window.close()    
+    
     def show_trainer_window(self):
         self.trainer_window.show()
+        self.menu_window.close()
+        
+    def show_report_window(self):
+        self.report_window.show()
         self.menu_window.close()
             
     def show_main_window(self):
