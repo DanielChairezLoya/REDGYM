0f5a79a807ea39de02a671dc03d22a75684f04f1
DanielChairezLoya <danielex7788@gmail.com>
2026-05-21 14:51:28 -0600

    conexion exitosa

--- controllers/addM_controller.py
+++ controllers/addM_controller.py
@@ -1,24 +1,25 @@
 import sys
+
 from PyQt6 import QtWidgets, uic
 from conexion import Conexion
 class AddMemberController:
     def __init__(self, window, model):
         self.window = window
         self.model = model
-        #self.conexion=Conexion()
-        #self.conexion.conectar()
-        self.btn_add.clicked.connect(self.add_member)
+        self.conexion=Conexion()
+        self.conexion.conectar()
+        self.window.btn_addm.clicked.connect(self.add_member)
         
     def add_member(self):
         name = self.txtName.text()
         last = self.txtApellido.text()
-        email = self.txtNumber.text()
+        number = str(self.txtNumber.text())
         
-        if name.strip() == "" or last.strip() == "" or email.strip() == "":
+        if name.strip() == "" or last.strip() == "" or number.strip() == "":
             QtWidgets.QMessageBox.warning(self, "Favor de llenar todos los campos")   
         else:
-            sql= "INSERT INTO users values (%s,%s,%s,%s,%s,%s)"
-            valores = (0,name,last,email,'default.png')
+            sql= "INSERT INTO users values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            valores = ('default.png',0,name,last,number)
             self.conexion.insertar(sql,valores)
             QtWidgets.QMessageBox.information(self,"registro insertado")    
     
